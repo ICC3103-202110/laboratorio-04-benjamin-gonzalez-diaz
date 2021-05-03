96183996155ecab95dc630a79ffb3967c7118fe3
benjamin gonzalez <bigonzalez@miuandes.cl>
2021-05-03 13:58:23 -0400

    creando funciones :)

--- lab4.js
+++ lab4.js
@@ -1,2 +1,24 @@
 console.log("hola")
+function View(counter){
+
+
+
+
+}
+function update(sign,counter){
+    if(sign == '+'){
+        return counter+1
+    }
+    if(sign == '-'){
+        return counter-1
+    }
+    else{
+        return counter
+    }
+}
+function App(counter){
+
+
+
+}
 //node lab4.js
