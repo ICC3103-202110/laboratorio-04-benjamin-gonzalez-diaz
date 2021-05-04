2f8595a3c70b588349eab1606694f7faa47c94f8
benjamin gonzalez <bigonzalez@miuandes.cl>
2021-05-04 14:21:38 -0400

    lab parcialmente finalizado

--- lab4.js
+++ lab4.js
@@ -1,25 +1,34 @@
-const ps = require("prompt-sync");
-const prompt = ps();
+//const ps = require("prompt-sync");
+//const prompt = ps();
+const prompt = require('prompt-sync')();
 
-let name = prompt("Enter your name: ");
-console.log(`Hello ${name}`)
 function View(counter){
-    return ('Contador: '+counter+' \n (+)(-)\n(q)');
+    return ('Contador: '+counter);
 }
 function update(sign,counter){
-    if(sign == '+'){
-        return counter+1;
+    if(sign === '+'){
+        return counter += 1;
     }
-    if(sign == '-'){
-        return counter-1;
+    if(sign === '-'){
+        return counter -= 1;
     }
     else{
         return counter;
     }
 }
 function App(counter){
-
-
-
+    while(true){
+        first = true
+        const actualView = View(counter);
+        console.clear();
+        console.log(actualView);
+        let sign = prompt("que quiere hacer: \n\n(+)(-)\n(q)\n\n");
+        if(sign == 'q'){
+            break;
+        }
+        counter = update(sign,counter);
+    }
 }
+
+(App(0));
 //node lab4.js
